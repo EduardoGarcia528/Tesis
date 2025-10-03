48c4e663e5f754a7914fd7fce7b40b69aee6c884
EduardoGarcia528 <garciacastrejon528@gmail.com>
2025-10-02 22:02:06 -0600

    ising

--- FEC/ising_compute.py
+++ FEC/ising_compute.py
@@ -12,70 +12,80 @@ def red_inicial(L, p):
     red[U < p] = -1
     return red
 
-def get_energy(red, J=1):
-    kernel = generate_binary_structure(2,1,)
-    kernel[1,1] = False
-    energia = J*np.sum(-red * convolve(red, kernel, mode='wrap'))
-    return energia // 2  # cada par contado dos veces
+def get_energy(red):
+    kernel = generate_binary_structure(2,1)
+    kernel[1,1] = 0
+    energia = np.sum(red * convolve(red, kernel, mode='wrap'))
+    return -0.5*energia  # cada par contado dos veces
+    # E/J
 
 #Metropolis
 
 @njit
-def metropolis(red, time_steps,BJ, energy, J =1):
+def metropolis(red, time_steps,BJ, energy):
     red = red.copy()
+    Lx, Ly = red.shape
     magnetizacion = np.zeros(time_steps)
     energia = np.zeros(time_steps)
+    M = np.sum(red)
     for t in range(time_steps):
-        print(t)
-        x = np.random.randint(red.shape[0])
-        y = np.random.randint(red.shape[1])
+        if t in [time_steps//4, time_steps//2, 3*time_steps//4]:
+            print(t)
+        x = np.random.randint(0,Lx)
+        y = np.random.randint(0,Ly)
         spin_i = red[x,y]
-
-        E_i = -J*spin_i*(red[x-1,y] + red[x,y-1] + red[x+1,y] + red[x,y+1])
-
+        xm = x - 1 if x > 0 else Lx - 1
+        xp = x + 1 if x < Lx - 1 else 0
+        ym = y - 1 if y > 0 else Ly - 1
+        yp = y + 1 if y < Ly - 1 else 0
+        E_i = spin_i*(red[xm, y] + red[xp, y] + red[x, ym] + red[x, yp])
         delta_E = 2*E_i
 
         if delta_E <= 0:
             red[x,y] *= -1
             energy += delta_E
+            M += -2*spin_i
         else:
             r = np.random.random()
             if r < np.exp(-BJ*delta_E):
                 red[x,y] *= -1
                 energy += delta_E
+                M += -2*spin_i
 
-        magnetizacion[t] = np.sum(red)
+        magnetizacion[t] = M/(Lx*Ly)
         energia[t] = energy
     return magnetizacion, energia
 
 
-def simulate_ising(L, p, time_steps, T, J=1):
+def simulate_ising(L, p, time_steps, BJ):
     red = red_inicial(L, p)
-    energy = get_energy(red, J=J)
-    BJ = 1.0 / T # Kb = 1, T en unidades de J, entonces BJ = 1/T
-    magnetizacion, energia = metropolis(red, time_steps, BJ, energy, J=J)
+    energy = get_energy(red)
+    # BJ = 1.0 / T # Kb = 1, T en unidades de J, entonces BJ = 1/T
+    magnetizacion, energia = metropolis(red, time_steps, BJ, energy)
+    print("ya")
     return magnetizacion, energia
 
 if __name__ == "__main__":
-    L = 1000
+    L = 100
     p = 0.75
-    time_steps = 100_000
-    T = 2.269
-    J = 1
+    time_steps  = 10_200_000
+    T = 2.269  #T critico ~ 2.269
+    # T = 3.0
+    BJ = 1.0 / T
 
-    magnetizaciones, energias = simulate_ising(L, p, time_steps, T, J=J)
-    np.save("magnetizaciones.npy", magnetizaciones)
-    np.save("energias.npy", energias)
+    magnetizaciones, energias = simulate_ising(L, p, time_steps, BJ)
+    np.save("magnetizaciones.npy", magnetizaciones[200_000:])
+    np.save("energias.npy", energias[200_000:])
 
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    plt.plot(magnetizaciones)
+    plt.plot(magnetizaciones[200_000:])
     plt.title('Magnetización vs Tiempo')
     plt.xlabel('Tiempo')
     plt.ylabel('Magnetización')
 
     plt.subplot(1, 2, 2)
-    plt.plot(energias)
+    plt.plot(energias[200_000:])
     plt.title('Energía vs Tiempo')
     plt.xlabel('Tiempo')
     plt.ylabel('Energía')
